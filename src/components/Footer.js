ddd4b217f656ab8ffa766181302078e9978c6393
LiamGuan <635444435@qq.com>
2023-02-05 17:57:58 -0600

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,11 @@ import Logo from '../img/Real_Madrid_2.png'
 const Footer = () => {
   return <div className='footer'>
     <img src={Logo} />
+    <div>
+      <a href='https://github.com/Probabilitys' target="_blank">Probabilitys</a>|
+      <a href='https://github.com/Probabilitys/real-madrid' target="_blank">GitHub</a>
+    </div>
+    
   </div>;
 };
 
